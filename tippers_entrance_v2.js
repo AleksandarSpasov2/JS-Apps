f1c7f0f426c5b3b62d06f4cdd167b8244bb3483f
Aleksandar Spasov <147007999+aleksandarspasovv@users.noreply.github.com>
2024-10-20 14:43:33 +0300

    Update tippers_entrance_v2.js

--- tippers_entrance_v2.js
+++ tippers_entrance_v2.js
@@ -5,18 +5,7 @@ var specialUsers = ["User1", "User2", "User3"];  // Replace with actual username
 cb.onEnter(function(user) {
     // Check if the entered user is in the specialUsers list
     if (specialUsers.includes(user['user'])) {
-        // Send a private notice only to the broadcaster
-        cb.sendNotice("User " + user['user'] + " has entered the room.", cb.room_slug, null);
-        
-        // Send an additional informational notice to the broadcaster like in the first program
-        Messenger.sendInfoMessage(user['user'] + " has entered the room.", cb.room_slug, null);
+        // Send a private notice only to the broadcaster with a dark purple color
+        cb.sendNotice("User " + user['user'] + " has entered the room.", cb.room_slug, null, "#9400D3", 'bold');
     }
 });
-
-// Ensure Messenger object is defined as in the first program
-
-var Messenger = {
-    sendInfoMessage: function (str, recipient, group) {
-        cb.sendNotice(str, recipient, null, "#000000", 'bold', group);
-    }
-};
